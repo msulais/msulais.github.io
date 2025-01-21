d86b169842fd8977bb315ed3b190685fa9c0218b
Muhammad Sulais <muh.sulais@gmail.com>
2025-01-21 22:47:15 +0700

    responsive header

--- scripts/header/navigation.js
+++ scripts/header/navigation.js
@@ -10,8 +10,12 @@ let selected_page = document.querySelector('main>[data-selected]')
 function update_page(pathname, animation=true){
 	if (pathname == selected_route) return
 
-	document.querySelector('nav>[data-selected]').removeAttribute('data-selected')
-	document.querySelector(`nav>[href="${CSS.escape(pathname)}"]`).setAttribute('data-selected', '')
+	for (const el of document.querySelectorAll('header>:is(nav,dialog)>[data-selected]')){
+		el.removeAttribute('data-selected')
+	}
+	for (const el  of document.querySelectorAll(`header>:is(nav,dialog)>[href="${CSS.escape(pathname)}"]`)){
+		el.setAttribute('data-selected', '')
+	}
 
 	selected_route = pathname
 	selected_page.removeAttribute('data-selected')
@@ -51,7 +55,29 @@ function init_custom_btn() {
 	btn.onclick = () => update_page('/contact')
 }
 
+function init_dialog_navigation() {
+	const menu_btn = document.getElementById('h-menu')
+
+	/** @type HTMLDialogElement */
+	const navigation_dialog = document.getElementById('h-dialog')
+	const close_dialog_btn = document.getElementById('h-dialog-close')
+
+	menu_btn.onclick = () => navigation_dialog.showModal()
+	close_dialog_btn.onclick = () => navigation_dialog.close()
+	navigation_dialog.onclick = (ev) => {
+		ev.preventDefault()
+
+		/** @type HTMLAnchorElement */
+		const target = document.activeElement
+		if (target.tagName !== 'A') return
+
+		update_page(target.getAttribute('href'))
+		navigation_dialog.close()
+	}
+}
+
 init_navigation()
 init_navigation_btn()
 init_custom_btn()
+init_dialog_navigation()
 })()
