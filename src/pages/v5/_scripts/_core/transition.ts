ec29a816f20d26cbcad72a3de380e433fe67b7ac
Muhammad Sulais <muh.sulais@gmail.com>
2026-01-03 16:36:58 +0700

    [V5] fix easing

--- src/pages/v5/_scripts/_core/transition.ts
+++ src/pages/v5/_scripts/_core/transition.ts
@@ -7,48 +7,47 @@ export namespace PageTransition {
 	const clip_bottom = 'xywh(0% 100% 100% 0%)'
 	const clip_left = 'xywh(0% 0% 0% 100%)'
 	const clip_right = 'xywh(100% 0% 0% 100%)'
-	const options = {
-		duration: 500,
-		easing: 'cubic-bezier(.25,0,0,1)'
-	}
 	const fn_property = (clipPath?: string) => (clipPath
 		? ref_transition.style.setProperty('clip-path', clipPath)
 		: ref_transition.style.removeProperty('clip-path')
 	)
-	const fn_animate = (...clipPath: string[]) => (ref_transition
-		.animate({clipPath}, options)
+	const fn_animate = (toFull: boolean, ...clipPath: string[]) => (ref_transition
+		.animate({clipPath}, {
+			duration: 500,
+			easing: toFull? 'cubic-bezier(1,0,0.75,1)' : 'cubic-bezier(.25,0,0,1)'
+		})
 		.finished
 	)
 
 	export async function topToFull(): Promise<Animation> {
-		return fn_property(), fn_animate(clip_top, clip_full)
+		return fn_property(), fn_animate(true, clip_top, clip_full)
 	}
 
 	export async function bottomToFull(): Promise<Animation> {
-		return fn_property(), fn_animate(clip_bottom, clip_full)
+		return fn_property(), fn_animate(true, clip_bottom, clip_full)
 	}
 
 	export async function leftToFull(): Promise<Animation> {
-		return fn_property(), fn_animate(clip_left, clip_full)
+		return fn_property(), fn_animate(true, clip_left, clip_full)
 	}
 
 	export async function rightToFull(): Promise<Animation> {
-		return fn_property(), fn_animate(clip_right, clip_full)
+		return fn_property(), fn_animate(true, clip_right, clip_full)
 	}
 
 	export async function fullToBottom(): Promise<Animation> {
-		return fn_property(clip_bottom), fn_animate(clip_full, clip_bottom)
+		return fn_property(clip_bottom), fn_animate(false, clip_full, clip_bottom)
 	}
 
 	export async function fullToTop(): Promise<Animation> {
-		return fn_property(clip_top), fn_animate(clip_full, clip_top)
+		return fn_property(clip_top), fn_animate(false, clip_full, clip_top)
 	}
 
 	export async function fullToLeft(): Promise<Animation> {
-		return fn_property(clip_left), fn_animate(clip_full, clip_left)
+		return fn_property(clip_left), fn_animate(false, clip_full, clip_left)
 	}
 
 	export async function fullToRight(): Promise<Animation> {
-		return fn_property(clip_right), fn_animate(clip_full, clip_right)
+		return fn_property(clip_right), fn_animate(false, clip_full, clip_right)
 	}
 }
