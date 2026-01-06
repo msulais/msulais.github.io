a8f101b1b367ab3da67331b002d0f52e6212450b
Muhammad Sulais <muh.sulais@gmail.com>
2026-01-06 12:02:28 +0700

    update projects

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -1,6 +1,8 @@
 import { marked } from 'marked'
 import {rawContent as redmerah_desc} from '../projects/redmerah.md'
 import {rawContent as artic_desc} from '../projects/artic.md'
+import {rawContent as flutter_desc} from '../projects/flutter-apps.md'
+import {rawContent as loripsum_desc} from '../projects/loripsum.md'
 import redmerah_img1 from '../assets/images/projects/redmerah/redmerah-1.png'
 import redmerah_img2 from '../assets/images/projects/redmerah/redmerah-2.png'
 import redmerah_img3 from '../assets/images/projects/redmerah/redmerah-3.png'
@@ -10,6 +12,11 @@ import redmerah_img6 from '../assets/images/projects/redmerah/redmerah-6.png'
 import artic_img1 from '../assets/images/projects/artic/artic-1.png'
 import artic_img2 from '../assets/images/projects/artic/artic-2.png'
 import artic_img3 from '../assets/images/projects/artic/artic-3.png'
+import flutter_img1 from '../assets/images/projects/flutter-apps/flutter-1.png'
+import flutter_img2 from '../assets/images/projects/flutter-apps/flutter-2.png'
+import flutter_img3 from '../assets/images/projects/flutter-apps/flutter-3.png'
+import loripsum_img1 from '../assets/images/projects/loripsum/loripsum-1.png'
+import loripsum_img2 from '../assets/images/projects/loripsum/loripsum-2.png'
 
 export namespace C_Projects {
 	export type Project = {
@@ -47,8 +54,45 @@ export namespace C_Projects {
 		description: marked(artic_desc(), {async: false})
 	}
 
+	export const flutterApps: Project = {
+		name: 'Flutter Apps',
+		urls: [
+			'https://github.com/msulais/flutter-sensors',
+			'https://github.com/msulais/flutter-tasks',
+			'https://github.com/msulais/flutter-baterai',
+			'https://github.com/msulais/flutter-randomizer',
+			'https://github.com/msulais/flutter-stopwatch',
+			'https://github.com/msulais/flutter-calculator',
+			'https://github.com/msulais/flutter-notes',
+			'https://github.com/msulais/flutter-voice_recorder'
+		],
+		images: [
+			[flutter_img1.src, 'All my flutter apps'],
+			[flutter_img2.src, 'Apps schreenshot'],
+			[flutter_img3.src, 'Apps schreenshot'],
+		],
+		technology: ['Flutter'],
+		description: marked(flutter_desc(), {async: false})
+	}
+
+	export const loripsum: Project = {
+		name: 'Loripsum / Figma Plugin',
+		urls: [
+			'https://www.figma.com/community/plugin/1539232405802126137/loripsum',
+			'https://github.com/msulais/figma-plugin-loripsum'
+		],
+		images: [
+			[loripsum_img1.src, 'Loripsum'],
+			[loripsum_img2.src, 'Plugin screenshot']
+		],
+		technology: ['Figma', 'Typescript'],
+		description: marked(loripsum_desc(), {async: false})
+	}
+
 	export const all: Project[] = [
 		redmerah,
-		artic
+		artic,
+		loripsum,
+		flutterApps
 	]
 }
---
+++ src/projects/loripsum.md
@@ -0,0 +1,12 @@
+A Figma plugin designed to generate truly randomized placeholder text.
+
+I built this plugin to solve a specific frustration I encountered during design work: the repetitive
+nature of existing text generators. While most tools output the standard Latin sequence ("*Lorem*
+*ipsum dolor sit amet...*") in the exact same order every time, my plugin implements a randomization
+algorithm to create unique text variations every time.
+
+**Key Features:**
+
+* **True Randomization:** Never get the same text sequence twice.
+* **Granular Control:** Generate specific amounts of Words, Sentences, or Paragraphs.
+* **Text Styling:** Automatically apply Sentence Case (Capitalize), UPPERCASE, or lowercase formatting.
