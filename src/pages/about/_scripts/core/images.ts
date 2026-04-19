4f63b2b3e3caa3e8ceb8011c161f611db00b0fec
Muhammad Sulais <sulaismuhammad@gmail.com>
2026-04-20 00:09:25 +0700

    zoom in image

---
+++ src/pages/about/_scripts/core/images.ts
@@ -0,0 +1,61 @@
+import * as SharedIds from '../shared/ids.enum'
+import { elementById, setElementStyle } from '../utils/dom'
+
+const SPRING_EASING = 'cubic-bezier(.25,0,0,1)'
+const _ref_project = elementById(SharedIds.ProjectSection)
+const _ref_dialog = document.createElement('dialog')
+let _selected_ref: HTMLImageElement | null = null
+
+function _insertDialog(): void {
+	setElementStyle(_ref_dialog, 'border', 'none')
+	setElementStyle(_ref_dialog, 'outline', 'none')
+	setElementStyle(_ref_dialog, 'margin', 'auto')
+	setElementStyle(_ref_dialog, 'cursor', 'zoom-out')
+	document.body.append(_ref_dialog)
+}
+
+function _initEvents(): void {
+	_ref_dialog.addEventListener('close', () => {
+		_selected_ref?.remove()
+		_selected_ref = null
+		document.body.style.removeProperty('overflow')
+	})
+
+	_ref_dialog.addEventListener('click', () => {
+		_selected_ref?.remove()
+		_selected_ref = null
+		_ref_dialog.close()
+	})
+
+	_ref_project?.addEventListener('click', (ev) => {
+		if (_selected_ref) {
+			_selected_ref.remove()
+			_selected_ref = null
+			_ref_dialog.close()
+		}
+		else {
+			const target = ev.target as HTMLImageElement
+			if (!(ev.target instanceof HTMLImageElement)) {
+				return
+			}
+
+			_selected_ref = target.cloneNode(true) as HTMLImageElement
+			setElementStyle(_selected_ref, 'width', '100%')
+			setElementStyle(_selected_ref, 'object-fit', 'contain')
+			_ref_dialog.replaceChildren(_selected_ref)
+			_ref_dialog.showModal()
+			setElementStyle(document.body, 'overflow', 'clip')
+			_ref_dialog.animate({
+				scale: [0.85, 1],
+				opacity: [0, 1]
+			}, {duration: 250, easing: SPRING_EASING})
+		}
+	})
+}
+
+function initImages(): void {
+	_initEvents()
+	_insertDialog()
+}
+
+export default initImages
--- src/pages/about/_scripts/index.ts
+++ src/pages/about/_scripts/index.ts
@@ -1,7 +1,9 @@
 import initFooter from "./core/footer";
 import initHeader from "./core/header";
+import initImages from "./core/images";
 import initSplash from "./core/splash";
 
 initSplash()
 initHeader()
 initFooter()
+initImages()
